ddb7e6f301ef6e9b445cbf3311d9439f8927f65c
muzaffarkhuja <25102006omr@gmail.com>
2023-06-15 13:51:17 +0500

    project started

---
+++ src/main/java/uz/omr/variant/VariantApplication.java
@@ -0,0 +1,13 @@
+package uz.omr.variant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VariantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VariantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/omr/variant/VariantApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.omr.variant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VariantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
